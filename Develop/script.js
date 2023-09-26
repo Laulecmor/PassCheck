d4e1960180d63a6d1204830981b757f40ef29cd7
Laulecmor <lauren.morrison00@gmail.com>
2023-09-25 21:28:52 -0500

    push

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ document.querySelector("#generate").addEventListener("click", writePassword);
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
 // Declaration 
 var confirmLength = "";
@@ -34,7 +34,7 @@ function generatePassword() {
   var passwordCharacters = []
 
   if (confirmSpecialCharacter) {
-    passwordCharacters = passwordCharacters.concat(specialChar)
+    passwordCharacters = passwordCharacters.concat(special)
   }
 
   if (confirmNumericCharacter) {
@@ -52,13 +52,13 @@ function generatePassword() {
   console.log(passwordCharacters)
 
   // A for loop so "generate password" will return random characters from each of the array varibles on line 5-8. 
-  var newPassword = ""
+  var randomPassword = ""
 
-  for (var i = 0; i < passwordLength; i++) {
-    newPassword = newPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-    console.log(newPassword)
+  for (var i = 0; i < confirmLength; i++) {
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(randomPassword)
   }
-  return newPassword;
+  return randomPassword;
 }
 
 // Write password to the #password input (given to us in the prework)
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Password Generator Starter Code
 # PassCheck
+
+This was a project so users could generate a new password - with the option to include chracters like numbers, uppercase letters, lower case letters, and special chracters. 
+
+In the script.js I created arrays for the characters that could be included in the password then after making some declarations I wrote a function so that once hitting "generate password" on the page, you will be prompted to decide all of the customization availible for a password. 
+
+I didn't have to touch the CSS or the HTML page. 
+
+![
+    
+](<Screenshot 2023-09-25 at 9.16.54 PM.png>)
+![ 
+
+](<Screenshot 2023-09-25 at 9.17.06 PM-1.png>)
+![
+    
+](<Screenshot 2023-09-25 at 9.17.20 PM.png>)
